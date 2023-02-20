90c7fb4229c11a0e2b8666ccf781e69f8773d8fe
Askar Oralkhan <askar4ik123@gmail.com>
2023-02-20 14:13:22 +0600

    firsta commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer sprite;
+    BoxCollider2D coll;
+    float dirX = 0f;
+    [SerializeField] float moveSpeed = 7f;
+    [SerializeField] float jumpForce = 14f;
+    [SerializeField] LayerMask jumpabaleGround;
+
+    enum MovementState { idle, running, jumping, falling }
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        UpdateAnimationState();
+    }
+
+    void UpdateAnimationState()
+    {
+        MovementState state;
+        if (dirX > 0)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        else if (dirX < 0)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if(rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+        anim.SetInteger("state", (int)state);
+    }
+    bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpabaleGround);
+    }
+}
